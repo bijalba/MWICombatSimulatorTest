13a0df5f62bf940736f55b47369289f8d6ce1261
AmVoidGuy <132288247+AmVoidGuy@users.noreply.github.com>
2025-04-24 16:52:26 -0400

    Regen fix

--- dist/src_worker_js.bundle.js
+++ dist/src_worker_js.bundle.js
@@ -821,12 +821,12 @@ class CombatSimulator extends EventTarget {
                 continue;
             }
 
-            let hitpointRegen = Math.floor(unit.combatDetails.maxHitpoints * unit.combatDetails.combatStats.HPRegen);
+            let hitpointRegen = Math.floor(unit.combatDetails.maxHitpoints * unit.combatDetails.combatStats.hpRegenPer10);
             let hitpointsAdded = unit.addHitpoints(hitpointRegen);
             this.simResult.addHitpointsGained(unit, "regen", hitpointsAdded);
             // console.log("Added hitpoints:", hitpointsAdded);
 
-            let manapointRegen = Math.floor(unit.combatDetails.maxManapoints * unit.combatDetails.combatStats.MPRegen);
+            let manapointRegen = Math.floor(unit.combatDetails.maxManapoints * unit.combatDetails.combatStats.mpRegenPer10);
             let manapointsAdded = unit.addManapoints(manapointRegen);
             this.simResult.addManapointsGained(unit, "regen", manapointsAdded);
             // console.log("Added manapoints:", manapointsAdded);
@@ -1607,8 +1607,8 @@ class CombatUnit {
             natureResistance: 0,
             fireResistance: 0,
             lifeSteal: 0,
-            HPRegen: 0.01,
-            MPRegen: 0.01,
+            hpRegenPer10: 0.01,
+            mpRegenPer10: 0.01,
             combatDropRate: 0,
             combatDropQuantity: 0,
             combatRareFind: 0,
@@ -1645,15 +1645,15 @@ class CombatUnit {
 
     updateCombatDetails() {
         if (this.isPlayer) {
-            if (this.combatDetails.combatStats.HPRegen === 0) {
-                this.combatDetails.combatStats.HPRegen = 0.01;
+            if (this.combatDetails.combatStats.hpRegenPer10 === 0) {
+                this.combatDetails.combatStats.hpRegenPer10 = 0.01;
             } else {
-                this.combatDetails.combatStats.HPRegen = 0.01 + this.combatDetails.combatStats.HPRegen;
+                this.combatDetails.combatStats.hpRegenPer10 = 0.01 + this.combatDetails.combatStats.hpRegenPer10;
             }
-            if (this.combatDetails.combatStats.MPRegen === 0) {
-                this.combatDetails.combatStats.MPRegen = 0.01;
+            if (this.combatDetails.combatStats.mpRegenPer10 === 0) {
+                this.combatDetails.combatStats.mpRegenPer10 = 0.01;
             } else {
-                this.combatDetails.combatStats.MPRegen = 0.01 + this.combatDetails.combatStats.MPRegen;
+                this.combatDetails.combatStats.mpRegenPer10 = 0.01 + this.combatDetails.combatStats.mpRegenPer10;
             }
         }
 
@@ -1779,12 +1779,12 @@ class CombatUnit {
         }
 
         let hpRegenBoosts = this.getBuffBoost("/buff_types/hp_regen");
-        this.combatDetails.combatStats.HPRegen += this.combatDetails.combatStats.HPRegen * hpRegenBoosts.ratioBoost;
-        this.combatDetails.combatStats.HPRegen += hpRegenBoosts.flatBoost;
+        this.combatDetails.combatStats.hpRegenPer10 += this.combatDetails.combatStats.hpRegenPer10 * hpRegenBoosts.ratioBoost;
+        this.combatDetails.combatStats.hpRegenPer10 += hpRegenBoosts.flatBoost;
 
         let mpRegenBoosts = this.getBuffBoost("/buff_types/mp_regen");
-        this.combatDetails.combatStats.MPRegen += this.combatDetails.combatStats.MPRegen * mpRegenBoosts.ratioBoost;
-        this.combatDetails.combatStats.MPRegen += mpRegenBoosts.flatBoost;
+        this.combatDetails.combatStats.mpRegenPer10 += this.combatDetails.combatStats.mpRegenPer10 * mpRegenBoosts.ratioBoost;
+        this.combatDetails.combatStats.mpRegenPer10 += mpRegenBoosts.flatBoost;
 
         this.combatDetails.combatStats.lifeSteal += this.getBuffBoost("/buff_types/life_steal").flatBoost;
         this.combatDetails.combatStats.physicalThorns += this.getBuffBoost(
@@ -3304,8 +3304,8 @@ class Monster extends _combatUnit__WEBPACK_IMPORTED_MODULE_1__["default"] {
                     "maxHitpoints",
                     "maxManapoints",
                     "lifeSteal",
-                    "HPRegen",
-                    "MPRegen",
+                    "hpRegenPer10",
+                    "mpRegenPer10",
                     "physicalThorns",
                     "elementalThorns",
                     "combatDropRate",
@@ -3388,8 +3388,8 @@ class Monster extends _combatUnit__WEBPACK_IMPORTED_MODULE_1__["default"] {
                     "maxHitpoints",
                     "maxManapoints",
                     "lifeSteal",
-                    "HPRegen",
-                    "MPRegen",
+                    "hpRegenPer10",
+                    "mpRegenPer10",
                     "physicalThorns",
                     "elementalThorns",
                     "combatDropRate",
@@ -3472,8 +3472,8 @@ class Monster extends _combatUnit__WEBPACK_IMPORTED_MODULE_1__["default"] {
                     "maxHitpoints",
                     "maxManapoints",
                     "lifeSteal",
-                    "HPRegen",
-                    "MPRegen",
+                    "hpRegenPer10",
+                    "mpRegenPer10",
                     "physicalThorns",
                     "elementalThorns",
                     "combatDropRate",
@@ -3641,8 +3641,8 @@ class Player extends _combatUnit__WEBPACK_IMPORTED_MODULE_1__["default"] {
             "maxHitpoints",
             "maxManapoints",
             "lifeSteal",
-            "HPRegen",
-            "MPRegen",
+            "hpRegenPer10",
+            "mpRegenPer10",
             "physicalThorns",
             "elementalThorns",
             "combatDropRate",
