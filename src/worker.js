bbb3ed429ac74751d438738506fcebe6d0dda23a
bijalba <bijalba@gmail.com>
2025-07-05 00:39:18 -0500

    This commit will be automatically published

---
+++ src/worker.js
@@ -0,0 +1,32 @@
+import CombatSimulator from "./combatsimulator/combatSimulator";
+import Player from "./combatsimulator/player";
+import Zone from "./combatsimulator/zone";
+
+
+onmessage = async function (event) {
+    switch (event.data.type) {
+        case "start_simulation":
+            let playersData = event.data.players;
+            let players = [];
+            let zone = new Zone(event.data.zoneHrid);
+            for (let i = 0; i < playersData.length; i++) {
+                let currentPlayer = Player.createFromDTO(structuredClone(playersData[i]));
+                currentPlayer.zoneBuffs = zone.buffs;
+                players.push(currentPlayer);
+            }
+            let simulationTimeLimit = event.data.simulationTimeLimit;
+            let combatSimulator = new CombatSimulator(players, zone);
+            combatSimulator.addEventListener("progress", (event) => {
+                this.postMessage({ type: "simulation_progress", progress: event.detail.progress, zone: event.detail.zone});
+            });
+
+            try {
+                let simResult = await combatSimulator.simulate(simulationTimeLimit);
+                this.postMessage({ type: "simulation_result", simResult: simResult });
+            } catch (e) {
+                console.log(e);
+                this.postMessage({ type: "simulation_error", error: e });
+            }
+            break;
+    }
+};
